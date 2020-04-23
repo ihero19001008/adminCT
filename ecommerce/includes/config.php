fbc4db5c0c31011085c53e781dd6425c494af707
= <anhld.bhsoft@gmail.com>
2020-04-23 08:12:46 +0700

    first commit

---
+++ ecommerce/includes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+    //database configuration
+    $host       = "localhost";
+    $user       = "root";
+    $pass       = "";
+    $database   = "ecommerce_android_app";
+
+    $connect = new mysqli($host, $user, $pass, $database);
+
+    if (!$connect) {
+        die ("connection failed: " . mysqli_connect_error());
+    } else {
+        $connect->set_charset('utf8');
+    }
+
+?>
